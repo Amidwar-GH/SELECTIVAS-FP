973410d2f27df883d692d942e05c69c34fbc1cbc
Amidwar <amidwar05@gmail.com>
2025-05-30 22:37:11 -0500

    Agregandoles sitema de peticion de nombre al usuario

--- SelectivasNotas.cpp
+++ SelectivasNotas.cpp
@@ -3,17 +3,20 @@ using namespace std;
 
 int main(){
 	float N1,N2,N3,SN,P; //nota uno nota dos nota tres suma de notas
-	cout<<Digite la primera nota: ; cin>>N1;
-	cout<<Digite la segunda nota: ; cin>>N2;
-	cout<<Digite la tercera nota: ; cin>>N3;
+	char nombre[50]; 
+
+    cout << "Digite su nombre: "; cin>> nombre;
+	cout<<"Digite la primera nota: "; cin>>N1;
+	cout<<"Digite la segunda nota: "; cin>>N2;
+	cout<<"Digite la tercera nota: "; cin>>N3;
 	
 	SN=N1+N2+N3;
 	P=SN/3;
 	
 	if(P>=70){ //OBSERVACION P>=70?
-		cout<<Estas aprobado;
+		cout<<nombre<<" Estas aprobado";
 	}else{
-		cout<<Estas desaprobado;
+		cout<<nombre<<" Estas desaprobado";
 	}
 	
 	
