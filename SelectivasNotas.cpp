d423cb578a4882289a832d997c5a3babe5d3e5a5
Amidwar <amidwar05@gmail.com>
2025-05-31 22:21:10 -0500

    Agregando sistema de antinegativos

--- SelectivasNotas.cpp
+++ SelectivasNotas.cpp
@@ -10,10 +10,20 @@ int main(){
 	cout<<"Digite la segunda nota: "; cin>>N2;
 	cout<<"Digite la tercera nota: "; cin>>N3;
 	
+	if(N1<0){
+		N1=N1*-1;
+	}
+	if(N2<0){
+		N2=N2*-1;
+	}
+	if(N3<0){
+		N3=N3*-1;
+	}
+	
 	SN=N1+N2+N3;
 	P=SN/3;
 	
-	if(P>=70){ //OBSERVACION P>=70?
+	if(P>=10.5){ //OBSERVACION P>=70?
 		cout<<nombre<<" Estas aprobado";
 	}else{
 		cout<<nombre<<" Estas desaprobado";
