fe58a6932601898a72a03c7f92b7bf2ad7a6a31e
Amidwar <amidwar05@gmail.com>
2025-06-01 20:34:59 -0500

    Modificando peticion de nombre a usuario

--- EjerAumentoBanco.cpp
+++ EjerAumentoBanco.cpp
@@ -3,27 +3,30 @@ using namespace std;
 
 int main(){
 	float TT,CA,NCA; //tipo de tarjeta // credito actual // nuevo credito aumentado
-	cout<<Digite el tipo de tarjeta: ; cin>>TT;
-	cout<<Digite su credito actual: ; cin>>CA;
+	char nombre[50];
+	cout<<"BIENVENIDO A MI PROGRAMA.";
+	cout<<"\n\nDigite su nombre: "; cin>>nombre;
+	cout<<"Digite el tipo de tarjeta: "; cin>>TT;
+	cout<<"Digite su credito actual: "; cin>>CA;
 	
 	if(TT==1){
 		NCA=CA+(CA*0.25);
-		cout<<El aumento de crédito es <<NCA<<endl;
+		cout<<nombre<<" el aumento de tu credito es "<<NCA<<endl;
 		return 0;
 	}
 	if(TT==2){
 		NCA=CA+(CA*0.35);
-		cout<<El aumento de crédito es <<NCA<<endl;
+		cout<<nombre<<" el aumento de tu credito es "<<NCA<<endl;
 		return 0;
 	}
 	if(TT==3){
 		NCA=CA+(CA*0.4);
-		cout<<El aumento de crédito es <<NCA<<endl;
+		cout<<nombre<<" el aumento de tu credito es "<<NCA<<endl;
 		return 0;
 	}
-	if(TT>3 || TT<1){   // || = o disyuncion tambien esta el &&
+	if(TT>3 || TT<1){   // || = "o" disyuncion tambien esta el &&
 		NCA=CA+(CA*0.5);
-		cout<<El aumento de crédito es <<NCA<<endl;
+		cout<<nombre<<" el aumento de tu credito es "<<NCA<<endl;
 		return 0;
 	}
 	
