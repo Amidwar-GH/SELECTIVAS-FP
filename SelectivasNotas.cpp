70c359a6055da40face3ed3abb7ba3b72c675981
JHONTONCONI <btaedwin123@gmail.com>
2025-06-04 00:53:43 -0400

    MEJORE LA INTERFAZ DEL EJERCICIO

--- SelectivasNotas.cpp
+++ SelectivasNotas.cpp
@@ -5,28 +5,32 @@ int main(){
 	float Nota1,Nota2,Nota3,SumaNotas,Promedio; //nota uno nota dos nota tres suma de notas
 	char nombre[50]; 
 
-    cout << "Digite su nombre: "; cin>> nombre;
+    cout<<"Digite su nombre: "; cin>> nombre;
 	cout<<"Digite la primera nota: "; cin>>Nota1;
 	cout<<"Digite la segunda nota: "; cin>>Nota2;
 	cout<<"Digite la tercera nota: "; cin>>Nota3;
 	
-	if(Nota1<0){
-		Nota1=Nota1*-1;
+	if(Nota1 < 0){
+		Nota1 = Nota1*-1;
 	}
-	if(Nota2<0){
-		Nota2=Nota2*-1;
+	if(Nota2 < 0){
+		Nota2 = Nota2*-1;
 	}
-	if(Nota3<0){
-		Nota3=Nota3*-1;
+	if(Nota3 < 0){
+		Nota3 = Nota3*-1;
 	}
 	
-	SumaNotas=Nota1+Nota2+Nota3;
-	Promedio=SumaNotas/3;
+	SumaNotas = Nota1+Nota2+Nota3;
+	Promedio = SumaNotas/3;
 	
-	if(Promedio>=10.5){ //OBSERVACION P>=70?
+	if(Promedio >= 10.5){ //OBSERVACION P>=70?
+	    cout<<endl;
 		cout<<nombre<<" Estas aprobado";
+		cout<<endl;
 	}else{
+		cout<<endl;
 		cout<<nombre<<" Estas desaprobado";
+		cout<<endl;
 	}
 	
 	
